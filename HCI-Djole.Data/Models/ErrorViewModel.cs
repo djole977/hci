99ec3417b4a1015f97742b058c083024f102e31d
Djordje <djordjedjoricsoko@gmail.com>
2023-06-14 15:03:41 +0200

    Init

---
+++ HCI-Djole.Data/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HCI_Djole.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
