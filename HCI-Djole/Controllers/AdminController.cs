8bd7e1dfacac32774381ce6fd080fb74c8db0843
DESKTOP-T87PFGJ\djord <djordjedjoricsoko@gmail.com>
2023-08-23 16:27:30 +0200

    Minor UI adjustments

---
+++ HCI-Djole/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HCI_Djole.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class AdminController : Controller
+    {
+        public IActionResult Settings()
+        {
+            return View();
+        }
+    }
+}
